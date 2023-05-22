e77ee7c2c591caf2de8a3ac5f8c9de4dd4efef1f
shareekaepps <shareekaepps@icloud.com>
2023-05-22 00:12:54 -0400

    added morgan package

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 // BACK-END APP
 const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("morgan")
+
 const blogs = require("./controllers/blogController.js");
 const blogsController = require("./controllers/blogsController.js");
-const app = express();
+
 
 // middleware
 app.use(express.json());  // parsing incoming data
 
 // routes
-app.use("/blogs", blogsController);
+app.use("/blogs", blogController);
 
 app.get("/", (req, res) => {
     res.send("Welcome to She Is Me App");
